beb034a2b92bcb259e40f981085766989bbdaa6c
Alexis <alexis.martinho.78@gmail.com>
2026-02-06 18:19:13 +0100

    Add Enhanced Input settings and UI assets for key mapping customization

---
+++ Source/CC_And_IF/MappableKeys/CCIF_InputUSerSettings.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UserSettings/EnhancedInputUserSettings.h"
+#include "CCIF_InputUSerSettings.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CC_AND_IF_API UCCIF_InputUSerSettings : public UEnhancedInputUserSettings
+{
+	GENERATED_BODY()
+};
---
+++ Source/CC_And_IF/MappableKeys/CCIF_PlayerMappableKeyProfile.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UserSettings/EnhancedInputUserSettings.h"
+#include "CCIF_PlayerMappableKeyProfile.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CC_AND_IF_API UCCIF_PlayerMappableKeyProfile : public UEnhancedPlayerMappableKeyProfile
+{
+	GENERATED_BODY()
+};
